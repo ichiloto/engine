a72acd1ea96b79d2b1bbe216cd629ca15b7d3644
Andrew Masiye <amasiye313@gmail.com>
2024-11-17 13:22:27 +0200

    feat(core): update Game.php

--- src/Core/Game.php
+++ src/Core/Game.php
@@ -2,44 +2,90 @@
 
 namespace Ichiloto\Engine\Core;
 
+use Assegai\Collections\ItemList;
+use Assegai\Util\Debug;
+use Exception;
 use Ichiloto\Engine\Core\Interfaces\CanRun;
+use Ichiloto\Engine\Events\Interfaces\EventInterface;
+use Ichiloto\Engine\Events\Interfaces\ObserverInterface;
+use Ichiloto\Engine\Events\Interfaces\StaticObserverInterface;
+use Ichiloto\Engine\Events\Interfaces\SubjectInterface;
+use Ichiloto\Engine\IO\Console\Console;
+use Ichiloto\Engine\IO\Enumerations\KeyCode;
+use Ichiloto\Engine\IO\Input;
+use Ichiloto\Engine\IO\InputManager;
 use Ichiloto\Engine\Scenes\Interfaces\SceneInterface;
 use Ichiloto\Engine\Scenes\SceneManager;
+use Ichiloto\Engine\Util\Config\ConfigStore;
+use Ichiloto\Engine\Util\Config\PlaySettings;
 
-class Game implements CanRun
+/**
+ * The game.
+ *
+ * @package Ichiloto\Engine\Core
+ */
+class Game implements CanRun, SubjectInterface
 {
-  protected bool $isRunning = false;
   /**
-   * The game options.
-   * @var array
+   * Whether the game is running.
+   * @var bool
    */
-  protected array $options = [];
-
+  protected bool $isRunning = false;
   /**
    * The scene manager.
    * @var SceneManager
    */
   protected SceneManager $sceneManager;
+  /**
+   * The input manager.
+   * @var InputManager
+   */
+  protected InputManager $inputManager;
+  /**
+   * The observers.
+   * @var ItemList<ObserverInterface>
+   */
+  protected ItemList $observers;
+  protected ItemList $staticObservers;
 
+  /**
+   * Game constructor.
+   *
+   * @param string $name The name of the game.
+   * @param int $width The width of the game screen.
+   * @param int $height The height of the game screen.
+   * @param array<string, mixed> $options The options to configure the game with.
+   */
   public function __construct(
     protected string $name,
     protected int $width = DEFAULT_SCREEN_WIDTH,
-    protected int $height = DEFAULT_SCREEN_HEIGHT
+    protected int $height = DEFAULT_SCREEN_HEIGHT,
+    protected array $options = []
   )
   {
-    $this->sceneManager = SceneManager::getInstance();
+    $this->declareManagers();
+    $this->declareObservers();
+  }
+
+  /**
+   * Game destructor.
+   */
+  public function __destruct()
+  {
+    Console::restoreTerminalSettings();
+    Console::reset();
   }
 
   /**
    * Configure the game.
    *
-   * @param array $options The options to configure the game with.
+   * @param array<string, mixed> $options The options to configure the game with.
    * @return Game
    */
   public function configure(array $options): self
   {
     $this->options = array_merge_recursive($this->options, $options);
-
+    ConfigStore::put(PlaySettings::class, new PlaySettings($this->options));
     return $this;
   }
 
@@ -61,15 +107,19 @@ class Game implements CanRun
    */
   public function run(): void
   {
-    $this->start();
+    try {
+      $this->start();
 
-    while ($this->isRunning) {
-      $this->handleInput();
-      $this->update();
-      $this->render();
+      while ($this->isRunning) {
+        $this->handleInput();
+        $this->update();
+        $this->render();
+      }
+
+      $this->stop();
+    } catch (Exception $exception) {
+      $this->handleException($exception);
     }
-
-    $this->stop();
   }
 
   /**
@@ -78,6 +128,8 @@ class Game implements CanRun
   protected function start(): void
   {
     // TODO: Implement start() method.
+
+    $this->isRunning = true;
   }
 
   /**
@@ -86,6 +138,8 @@ class Game implements CanRun
   protected function stop(): void
   {
     // TODO: Implement stop() method.
+
+    $this->isRunning = false;
   }
 
   /**
@@ -95,7 +149,7 @@ class Game implements CanRun
    */
   protected function handleInput(): void
   {
-
+    $this->inputManager->handleInput();
   }
 
   /**
@@ -105,6 +159,10 @@ class Game implements CanRun
    */
   protected function update(): void
   {
+    if (Input::isAnyKeyPressed([KeyCode::Q, KeyCode::q])) {
+      $this->stop();
+    }
+
     $this->sceneManager->update();
   }
 
@@ -116,5 +174,82 @@ class Game implements CanRun
   protected function render(): void
   {
     $this->sceneManager->render();
+  }
+
+  /**
+   * Declare the managers.
+   *
+   * @return void
+   */
+  private function declareManagers(): void
+  {
+    $this->sceneManager = SceneManager::getInstance();
+  }
+
+  /**
+   * Declare the observers.
+   *
+   * @return void
+   */
+  private function declareObservers(): void
+  {
+    $this->observers = new ItemList(ObserverInterface::class);
+    $this->staticObservers = new ItemList(StaticObserverInterface::class);
+  }
+
+  /**
+   * Handle an error.
+   *
+   * @param int $errno The error number.
+   * @param string $errstr The error string.
+   * @param string $errfile The error file.
+   * @param int $errline The error line.
+   * @return never
+   */
+  private function handleError(int $errno, string $errstr, string $errfile, int $errline): never
+  {
+    $this->stop();
+    exit($errno);
+  }
+
+  /**
+   * Handle an exception.
+   *
+   * @param Exception $exception The exception to handle.
+   * @return never
+   */
+  private function handleException(Exception $exception): never
+  {
+    Debug::error($exception);
+    $this->stop();
+    exit($exception->getMessage());
+  }
+
+  /**
+   * @inheritDoc
+   */
+  public function addObserver(ObserverInterface|string $observer): void
+  {
+    if ($observer instanceof ObserverInterface) {
+      $this->observers->add($observer);
+    } else if (is_a($observer, StaticObserverInterface::class, true)) {
+      $this->staticObservers->add($observer);
+    }
+  }
+
+  /**
+   * @inheritDoc
+   */
+  public function removeObserver(ObserverInterface|string $observer): void
+  {
+    // TODO: Implement removeObserver() method.
+  }
+
+  /**
+   * @inheritDoc
+   */
+  public function notify(object $entity, EventInterface $event): void
+  {
+    // TODO: Implement notify() method.
   }
 }
--- src/Rendering/Camera.php
+++ src/Rendering/Camera.php
@@ -5,11 +5,19 @@ namespace Ichiloto\Engine\Rendering;
 use Ichiloto\Engine\Core\Interfaces\CanRender;
 use Ichiloto\Engine\Core\Interfaces\CanResume;
 use Ichiloto\Engine\Core\Interfaces\CanStart;
+use Ichiloto\Engine\Scenes\Interfaces\SceneInterface;
 
 class Camera implements CanStart, CanResume, CanRender
 {
+  /**
+   * Camera constructor.
+   *
+   * @param SceneInterface $scene The scene that this camera is rendering.
+   */
   public function __construct(
-    protected
+    protected SceneInterface $scene,
+    protected int $width = DEFAULT_SCREEN_WIDTH,
+    protected int $height = DEFAULT_SCREEN_HEIGHT
   )
   {
   }
