72d20b0bdf25ddc03e49c95343482b61b1fc7676
Andrew Masiye <amasiye313@gmail.com>
2024-11-17 16:38:00 +0200

    feat(core): update Game.php

---
+++ src/Util/Config/ProjectConfig.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Ichiloto\Engine\Util\Config;
+
+use Ichiloto\Engine\Util\Config\AbstractConfig;
+
+class ProjectConfig extends AbstractConfig
+{
+
+  /**
+   * @inheritDoc
+   */
+  protected function load(): array
+  {
+    // TODO: Implement load() method.
+  }
+
+  /**
+   * @inheritDoc
+   */
+  public function persist(): void
+  {
+    // TODO: Implement persist() method.
+  }
+}
