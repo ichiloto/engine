e4b98991bdde9ef70b50d5c30c2c011773875463
Andrew Masiye <amasiye313@gmail.com>
2024-11-18 11:31:46 +0200

    feat(scenes): update scenes module

--- src/Scenes/AbstractScene.php
+++ src/Scenes/AbstractScene.php
@@ -4,8 +4,10 @@ namespace Ichiloto\Engine\Scenes;
 
 use Ichiloto\Engine\Core\GameObject;
 use Ichiloto\Engine\Rendering\Camera;
+use Ichiloto\Engine\Scenes\Interfaces\SceneInterface;
+use Ichiloto\Engine\UI\UIManager;
 
-class AbstractScene implements Interfaces\SceneInterface
+class AbstractScene implements SceneInterface
 {
   /**
    * The root game objects.
@@ -19,78 +21,34 @@ class AbstractScene implements Interfaces\SceneInterface
    * @var Camera
    */
   protected Camera $camera;
+  /**
+   * The UI manager.
+   *
+   * @var UIManager
+   */
+  protected UIManager $uiManager;
 
+  /**
+   * AbstractScene constructor.
+   *
+   * @param SceneManager $sceneManager The scene manager.
+   * @param string $name The name of the scene.
+   */
   public function __construct(
-    protected string $name
+    protected SceneManager $sceneManager,
+    protected string $name,
   )
   {
+    $this->uiManager = UIManager::getInstance();
     $this->camera = new Camera($this);
   }
 
   /**
    * @inheritDoc
    */
-  public function render(): void
+  public function getName(): string
   {
-    // TODO: Implement render() method.
-    foreach ($this->rootGameObjects as $gameObject) {
-      if ($gameObject->isActive()) {
-        $gameObject->render();
-      }
-    }
-  }
-
-  /**
-   * @inheritDoc
-   */
-  public function erase(): void
-  {
-    // TODO: Implement erase() method.
-    foreach ($this->rootGameObjects as $gameObject) {
-      if ($gameObject->isActive()) {
-        $gameObject->erase();
-      }
-    }
-  }
-
-  /**
-   * @inheritDoc
-   */
-  public function resume(): void
-  {
-    // TODO: Implement resume() method.
-  }
-
-  /**
-   * @inheritDoc
-   */
-  public function suspend(): void
-  {
-    // TODO: Implement suspend() method.
-  }
-
-  /**
-   * @inheritDoc
-   */
-  public function start(): void
-  {
-    // TODO: Implement start() method.
-  }
-
-  /**
-   * @inheritDoc
-   */
-  public function stop(): void
-  {
-    // TODO: Implement stop() method.
-  }
-
-  /**
-   * @inheritDoc
-   */
-  public function update(): void
-  {
-    // TODO: Implement update() method.
+    return $this->name;
   }
 
   /**
@@ -99,5 +57,99 @@ class AbstractScene implements Interfaces\SceneInterface
   public function getRootGameObjects(): array
   {
     return $this->rootGameObjects;
+  }
+
+  /**
+   * @inheritDoc
+   */
+  public function getUI(): UIManager
+  {
+    return $this->uiManager;
+  }
+
+  /**
+   * @inheritDoc
+   */
+  public function render(): void
+  {
+    $this->camera->render();
+  }
+
+  /**
+   * @inheritDoc
+   */
+  public function erase(): void
+  {
+    $this->camera->erase();
+  }
+
+  /**
+   * @inheritDoc
+   */
+  public function resume(): void
+  {
+    $this->camera->resume();
+
+    foreach ($this->rootGameObjects as $gameObject) {
+      if ($gameObject->isActive()) {
+        $gameObject->resume();
+      }
+    }
+  }
+
+  /**
+   * @inheritDoc
+   */
+  public function suspend(): void
+  {
+    $this->camera->suspend();
+
+    foreach ($this->rootGameObjects as $gameObject) {
+      if ($gameObject->isActive()) {
+        $gameObject->suspend();
+      }
+    }
+  }
+
+  /**
+   * @inheritDoc
+   */
+  public function start(): void
+  {
+    $this->camera->start();
+
+    foreach ($this->rootGameObjects as $gameObject) {
+      if ($gameObject->isActive()) {
+        $gameObject->start();
+      }
+    }
+  }
+
+  /**
+   * @inheritDoc
+   */
+  public function stop(): void
+  {
+    $this->camera->stop();
+
+    foreach ($this->rootGameObjects as $gameObject) {
+      if ($gameObject->isActive()) {
+        $gameObject->stop();
+      }
+    }
+  }
+
+  /**
+   * @inheritDoc
+   */
+  public function update(): void
+  {
+    $this->camera->update();
+
+    foreach ($this->rootGameObjects as $gameObject) {
+      if ($gameObject->isActive()) {
+        $gameObject->update();
+      }
+    }
   }
 }
